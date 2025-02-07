b084ab2dc9c57b32278e5c3c66d8c688c2ea401c
SwatiMishra8890 <mishraswati8890@gmail.com>
2025-02-07 09:42:34 +0530

    Add project files.

---
+++ jeecupregistration/Content/JS/regform.js
@@ -0,0 +1,55 @@
+﻿function SubmitForm()
+{
+    //GET FORM DATA///
+    var inputname = $("#inputname").val();
+    var fathername = $("#fathername").val();
+    var mothername = $("#mothername").val();
+    var date = $("#date").val();
+    var gender = $("#gender").val();
+    if (inputname == null || inputname == "") {
+        Swal.fire({
+            icon: "warning",
+            title: "warning...",
+            text: "Name is required",
+           
+        });
+        return;
+    }
+    if (fathername == null || fathername == "") {
+        Swal.fire({
+            icon: "warning",
+            title: "warning...",
+            text: "fathername is required",
+
+        });
+        return;
+    }
+    if (mothername == null || mothername == "") {
+        Swal.fire({
+            icon: "warning",
+            title: "warning...",
+            text: "mothername is required",
+
+        });
+        return;
+    }
+    if (date == null || date == "") {
+        Swal.fire({
+            icon: "warning",
+            title: "warning...",
+            text: "date is required",
+
+        });
+        return;
+    }
+    if (gender == null || gender == "") {
+        Swal.fire({
+            icon: "warning",
+            title: "warning...",
+            text: "gender is required",
+
+        });
+        return;
+    }
+   
+}
